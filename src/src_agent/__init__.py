b73e705cf202671482dd4e8932d27bb6770ddcc7
Chris <363708+christopherwoodall@users.noreply.github.com>
2025-07-13 17:22:23 -0500

    Update __init__.py

--- src/src_agent/__init__.py
+++ src/src_agent/__init__.py
@@ -2,9 +2,8 @@
 # Configure clean imports for the package
 # See: https://hynek.me/articles/testing-packaging/
 
-from . import src_agent
-
+from . import tools, src_agent
 from .src_agent import Agent
 
 
-__all__ = ["src_agent", "Agent"]
+__all__ = ["src_agent", "Agent", "tools"]
